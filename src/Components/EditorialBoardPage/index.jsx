ee5e84ec36126c99d33a040a8c426b243df7e76d
shreyansh95763 <tshreyansh36@gmail.com>
2026-02-10 10:52:27 +0530

    editor board

--- src/Components/EditorialBoardPage/index.jsx
+++ src/Components/EditorialBoardPage/index.jsx
@@ -20,12 +20,55 @@ const EditorialBoardPage = () => {
 
       <section className="content-section">
         <h2>Associate Editor</h2>
+
         <div className="profile-card slide-up">
           <h3>Er. Praveen Kumar Yadav</h3>
           <p>
             M.Tech (Environmental Engineering)
             <br />
             B.Tech (Civil Engineering)
+          </p>
+        </div>
+
+        <div className="profile-card slide-up">
+          <h3>Er. Vaishali Saxena</h3>
+          <p>
+            M.Tech (Biotechnology)
+            <br />
+            B.Tech (Biotechnology)
+          </p>
+        </div>
+
+        <div className="profile-card slide-up">
+          <h3>Parvez Alam Ansari</h3>
+          <p>
+            MBA (HR, Finance)
+            <br />
+            BBA
+          </p>
+        </div>
+
+        <div className="profile-card slide-up">
+          <h3>Er. Satyam Singh</h3>
+          <p>B.Tech (Computer Science Engineering)</p>
+        </div>
+
+        <div className="profile-card slide-up">
+          <h3>Er. Shreyansh Tripathi</h3>
+          <p>B.Tech (Computer Science Engineering)</p>
+        </div>
+
+        <div className="profile-card slide-up">
+          <h3>Er. Sayyed Md. Raja</h3>
+          <p>B.Tech (Mechanical Engineering)</p>
+        </div>
+
+        <div className="profile-card slide-up">
+          <h3>Satish Yadav</h3>
+          <p>
+            M.Sc. (Mathematics, Physics)
+            <br />
+            B.Sc., B.Ed.
           </p>
         </div>
       </section>
